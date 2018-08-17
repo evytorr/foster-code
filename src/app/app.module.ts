068d2fe5f40f0f48c0add189eadc976f0bbb0f6c
Evelyne Torres <arieletorres@gmail.com>
2018-08-17 13:01:19 -0700

    added profiles component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
+import { ProfilesComponent } from './profiles/profiles.component';
+// import { ScrollToModule } from 'ng2-scroll-to-el';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProfilesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
